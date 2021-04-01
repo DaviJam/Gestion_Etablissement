951302ff29f2bb861da7165440ed367c4f10df14
David <daori971@gmail.com>
2021-04-01 14:08:53 +0200

    update log4j

--- GestionEtablissement-logger/src/main/java/eu/ensup/gestionetablissement/logger/LoggerHandler.java
+++ GestionEtablissement-logger/src/main/java/eu/ensup/gestionetablissement/logger/LoggerHandler.java
@@ -1,14 +1,13 @@
 package eu.ensup.gestionetablissement.logger;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class LoggerHandler {
-    public boolean haveProperties = false;
-    public LoggerHandler() {
-        if (! haveProperties) {
-        	this.haveProperties = true;
-        	
-            PropertyConfigurator.configure(this.getClass().getResource("/log4j.properties"));
-        }
+
+    public LoggerHandler()
+    {
+        PropertyConfigurator.configure(this.getClass().getResource("/log4j.properties"));
     }
+
 }
